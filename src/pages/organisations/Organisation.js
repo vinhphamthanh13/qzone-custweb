810dfc601a89eb950a8a5b70666424665465ee9e
david.pham <david.vinhphamthanh@gmail.com>
2018-11-05 06:08:13 +0700

    [#2] Implement Organisation Page

---
+++ src/pages/organisations/Organisation.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+const Organisation = () => (
+  <Grid container>
+    <Paper>This is test Grid and Paper of Organisation And Now</Paper>
+  </Grid>
+);
+
+export default Organisation;
