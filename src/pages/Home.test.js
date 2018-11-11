b9802586f157768adcdaf31760ecde832ee0d869
Nhat Dang <dangquangnhat18101994@gmail.com>
2018-11-11 18:30:13 +0700

    add UT for Home and SubCategoryTabs

--- src/pages/Home.test.js
+++ src/pages/Home.test.js
@@ -1,10 +1,37 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Home from './Home';
+import CategoryTabs from './home/CategoryTabs';
+import SelectServices from './home/SelectServices';
 
 describe('Home', () => {
-  it('should render correctly', () => {
-    const wrapper = shallow(<Home />);
-    expect(wrapper.filter('div')).toHaveLength(1);
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<Home />);
+  });
+
+  it('should contain CategoryTabs and SelectServices', () => {
+    expect(wrapper.find(CategoryTabs)).toHaveLength(1);
+    expect(wrapper.find(SelectServices)).toHaveLength(1);
+  });
+
+  it('should pass props to CategoryTabs correctly', () => {
+    const selectedCategory = 'selectedCategory';
+
+    wrapper.setState({ selectedCategory });
+
+    expect(wrapper.find(CategoryTabs).prop('value')).toBe(selectedCategory);
+  });
+
+  it('should pass props to SelectServices correctly', () => {
+    const subCategories = 'subCategories';
+    const selectedSubCategory = 'selectedSubCategory';
+
+    wrapper.setState({ subCategories, selectedSubCategory });
+
+    expect(wrapper.find(SelectServices).prop('subCategories')).toBe(subCategories);
+    expect(wrapper.find(SelectServices).prop('selectedSubCategory'))
+      .toBe(selectedSubCategory);
   });
 });
---
+++ src/pages/home/SubCategoryTabs.test.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { Button } from '@material-ui/core';
+import SubCategoryTabs from './SubCategoryTabs';
+
+describe('SubCategoryTabs', () => {
+  const onChange = jest.fn();
+
+  beforeEach(() => {
+    onChange.mockReset();
+  });
+
+  it('should render 2 button and none of them is selected', () => {
+    const subCategories = [{ id: '1', name: 'sub-1' }, { id: '2', name: 'sub-2' }];
+    const selectedSubCategory = { id: '-1', name: '' };
+    const props = { subCategories, selectedSubCategory, onChange };
+
+    const wrapper = shallow(<SubCategoryTabs {...props} />);
+
+    expect(wrapper.find(Button)).toHaveLength(2);
+
+    const firstBtn = wrapper.find(Button).at(0);
+    expect(firstBtn.prop('variant')).toBe('text');
+    expect(firstBtn.children().text()).toBe('sub-1');
+
+    const secondBtn = wrapper.find(Button).at(1);
+    expect(secondBtn.prop('variant')).toBe('text');
+    expect(secondBtn.children().text()).toBe('sub-2');
+  });
+
+  it('should render 2 button and one of them is selected', () => {
+    const selectedSubCategory = { id: '1', name: 'sub-1' };
+    const subCategories = [selectedSubCategory, { id: '2', name: 'sub-2' }];
+    const props = { subCategories, selectedSubCategory, onChange };
+
+    const wrapper = shallow(<SubCategoryTabs {...props} />);
+
+    expect(wrapper.find(Button)).toHaveLength(2);
+
+    const firstBtn = wrapper.find(Button).at(0);
+    expect(firstBtn.prop('variant')).toBe('contained');
+    expect(firstBtn.children().text()).toBe('sub-1');
+
+    const secondBtn = wrapper.find(Button).at(1);
+    expect(secondBtn.prop('variant')).toBe('text');
+    expect(secondBtn.children().text()).toBe('sub-2');
+  });
+
+  it('should render 1 button with correct onChange prop', () => {
+    const selectedSubCategory = { id: '1', name: 'sub-1' };
+    const subCategories = [selectedSubCategory];
+    const props = { subCategories, selectedSubCategory, onChange };
+
+    const wrapper = shallow(<SubCategoryTabs {...props} />);
+    const btn = wrapper.find(Button).at(0);
+    btn.simulate('click');
+
+    expect(onChange).toHaveBeenCalledTimes(1);
+    expect(onChange).toHaveBeenCalledWith(selectedSubCategory, 'selectedSubCategory');
+  });
+});
