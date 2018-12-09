c848f0c5939d23e43ffbb1cb88be9e7ff6e3d0b3
Nhat Dang <dangquangnhat18101994@gmail.com>
2018-12-09 16:18:47 +0700

    Update dependencies

--- src/containers/App.js
+++ src/containers/App.js
@@ -7,7 +7,7 @@ import rootRoutes from 'config/routing/app';
 import store from 'config/store';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import MuiPickersUtilsProvider from 'material-ui-pickers/MuiPickersUtilsProvider';
-import DateFnsUtils from 'material-ui-pickers/utils/date-fns-utils';
+import DateFnsUtils from '@date-io/date-fns';
 import JssProvider from 'react-jss/lib/JssProvider';
 import { create } from 'jss';
 import { createGenerateClassName, jssPreset } from '@material-ui/core/styles';
