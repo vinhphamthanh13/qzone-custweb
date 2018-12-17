85e7657f5d53912f024558636d44b785d0091da5
Nhat Dang <dangquangnhat18101994@gmail.com>
2018-12-17 23:03:58 +0700

    remove mockProviders from ServiceCard

--- src/pages/home/services/ServiceCard.js
+++ src/pages/home/services/ServiceCard.js
@@ -9,40 +9,10 @@ import './ServiceCard.scss';
 import { serviceType } from 'types/global';
 import ServiceDetail from './serviceCard/ServiceDetail';
 
-const mockProviders = [
-  {
-    id: '1',
-    name: 'Provider 1',
-    availableSlots: [],
-  },
-  {
-    id: '2',
-    name: 'Provider 2',
-    availableSlots: [],
-  },
-  {
-    id: '3',
-    name: 'Provider 3',
-    availableSlots: [],
-  },
-];
-
 export default class ServiceCard extends PureComponent {
   static propTypes = {
     service: serviceType.isRequired,
     onChange: PropTypes.func.isRequired,
-  }
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      selectedTime: { providerId: '1', time: '06:00 PM' },
-      selectedDate: new Date(),
-    };
-  }
-
-  onSelectTime = (providerId, time) => {
-    this.setState({ selectedTime: { providerId, time } });
   }
 
   onSelectService = () => {
@@ -51,7 +21,6 @@ export default class ServiceCard extends PureComponent {
 
   render() {
     const { service } = this.props;
-    const { selectedTime, selectedDate } = this.state;
 
     return (
       <Card raised>
@@ -60,13 +29,7 @@ export default class ServiceCard extends PureComponent {
           image={serviceImg}
         />
         <CardContent>
-          <ServiceDetail
-            providers={mockProviders}
-            service={service}
-            selectedDate={selectedDate}
-            selectedTime={selectedTime}
-            onSelectTime={this.onSelectTime}
-          />
+          <ServiceDetail service={service} />
         </CardContent>
         <CardActions classes={{ root: 'service-card__footer' }}>
           <Button color="primary" variant="contained" onClick={this.onSelectService}>
--- src/pages/home/services/serviceCard/ServiceDetail.js
+++ src/pages/home/services/serviceCard/ServiceDetail.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import {
   Typography, Grid, Button,
   Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions,
 } from '@material-ui/core';
-import { serviceType, providerType } from 'types/global';
+import { serviceType } from 'types/global';
 import './ServiceDetail.scss';
 import CustomLink from 'components/CustomLink';
 
@@ -26,11 +25,8 @@ export default class ServiceDetail extends React.PureComponent {
   }
 
   render() {
-    const {
-      providers, service, selectedTime, onSelectTime,
-    } = this.props;
+    const { service } = this.props;
     const { isDialogDescOpen } = this.state;
-    const selectedProvider = providers.find(provider => provider.id === selectedTime.providerId);
 
     return (
       <React.Fragment>
@@ -45,16 +41,18 @@ export default class ServiceDetail extends React.PureComponent {
           </DialogContent>
           <DialogActions>
             <Button onClick={this.handleClose} color="primary" variant="outlined">
-                Close
+              Close
             </Button>
           </DialogActions>
         </Dialog>
-        <Typography variant="title">{service.name}</Typography>
+        <Typography align="center" variant="title">{service.name}</Typography>
         <div className="service-detail">
           <Typography variant="subtitle2">
             {(service.description || '').substring(0, 300)}...&nbsp;
             <CustomLink text="Read more" to="#" onClick={this.openDialog} />
           </Typography>
+        </div>
+        <div className="service-detail">
           <Grid container className="service-detail__item">
             <Grid item sm={6}>
               <Typography variant="caption">Duration:</Typography>
@@ -73,30 +71,6 @@ export default class ServiceDetail extends React.PureComponent {
               </Typography>
             </Grid>
           </Grid>
-          <Grid container className="service-detail__item">
-            <Grid item sm={6}>
-              <Typography variant="caption">Service provider:</Typography>
-            </Grid>
-            <Grid item sm={6}>
-              <Typography variant="subtitle2">
-                <CustomLink text={selectedProvider.name} to="#" />
-              </Typography>
-            </Grid>
-          </Grid>
-          <Grid container spacing={8} className="service-detail__item">
-            {selectedProvider.availableSlots.map(time => (
-              <Grid item sm={3} key={time}>
-                <Button
-                  variant={selectedTime.providerId === selectedProvider.id && selectedTime.time === time
-                    ? 'contained' : 'outlined'}
-                  color="secondary"
-                  onClick={() => { onSelectTime(selectedProvider.id, time); }}
-                >
-                  {time}
-                </Button>
-              </Grid>
-            ))}
-          </Grid>
         </div>
       </React.Fragment>
     );
@@ -104,12 +78,5 @@ export default class ServiceDetail extends React.PureComponent {
 }
 
 ServiceDetail.propTypes = {
-  providers: PropTypes.arrayOf(providerType).isRequired,
   service: serviceType.isRequired,
-  // selectedDate: PropTypes.instanceOf(Date).isRequired,
-  selectedTime: PropTypes.shape({
-    providerId: PropTypes.string.isRequired,
-    time: PropTypes.string.isRequired,
-  }).isRequired,
-  onSelectTime: PropTypes.func.isRequired,
 };
