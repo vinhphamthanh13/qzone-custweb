89319feb811f9d8828f722d414ed17b407327194
Shjn Ha Jung <shjnhajung@gmail.com>
2019-01-06 23:07:20 +0700

    hide date filter

--- src/pages/home/AdvancedSearch.js
+++ src/pages/home/AdvancedSearch.js
@@ -1,32 +1,32 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import './AdvancedSearch.scss';
-import DateTimePicker from 'material-ui-pickers/DateTimePicker';
+// import DateTimePicker from 'material-ui-pickers/DateTimePicker';
 
 export default class AdvancedSearch extends PureComponent {
   static propTypes = {
     onChange: PropTypes.func.isRequired,
   }
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      renderedFromDate: new Date(),
-      renderedToDate: new Date(),
-    };
-  }
-
-  onFromDateChange = (value) => {
-    const { onChange } = this.props;
-    this.setState({ renderedFromDate: value });
-    onChange(value, 'fromDate');
-  }
-
-  onToDateChange = (value) => {
-    const { onChange } = this.props;
-    this.setState({ renderedToDate: value });
-    onChange(value, 'toDate');
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     renderedFromDate: new Date(),
+  //     renderedToDate: new Date(),
+  //   };
+  // }
+  //
+  // onFromDateChange = (value) => {
+  //   const { onChange } = this.props;
+  //   this.setState({ renderedFromDate: value });
+  //   onChange(value, 'fromDate');
+  // }
+  //
+  // onToDateChange = (value) => {
+  //   const { onChange } = this.props;
+  //   this.setState({ renderedToDate: value });
+  //   onChange(value, 'toDate');
+  // }
 
   handleExpandClick = (categoryId) => {
     this.setState(state => ({
@@ -43,26 +43,31 @@ export default class AdvancedSearch extends PureComponent {
   }
 
   render() {
-    const {
-      renderedFromDate, renderedToDate,
-    } = this.state;
-
     return (
-      <React.Fragment>
-        <DateTimePicker
-          keyboard
-          label="From date"
-          className="advanced-search__from-date"
-          value={renderedFromDate}
-          onChange={this.onFromDateChange}
-        />
-        <DateTimePicker
-          keyboard
-          label="To date"
-          value={renderedToDate}
-          onChange={this.onToDateChange}
-        />
-      </React.Fragment>
+      <React.Fragment></React.Fragment>
     );
   }
+  // render() {
+  //   const {
+  //     renderedFromDate, renderedToDate,
+  //   } = this.state;
+  //
+  //   return (
+  //     <React.Fragment>
+  //       <DateTimePicker
+  //         keyboard
+  //         label="From date"
+  //         className="advanced-search__from-date"
+  //         value={renderedFromDate}
+  //         onChange={this.onFromDateChange}
+  //       />
+  //       <DateTimePicker
+  //         keyboard
+  //         label="To date"
+  //         value={renderedToDate}
+  //         onChange={this.onToDateChange}
+  //       />
+  //     </React.Fragment>
+  //   );
+  // }
 }
