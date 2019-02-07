59614543c0c392e41c79e866be43691be50f51d7
DavidPham <david.vinhphamthanh@gmail.com>
2019-02-07 13:25:06 +0700

    [UX/UI] Change loading color to light gray

--- src/components/Modal/Loading.js
+++ src/components/Modal/Loading.js
@@ -5,7 +5,7 @@ import { compose } from 'redux';
 import withStyles from '@material-ui/core/styles/withStyles';
 import { PropagateLoader } from 'react-spinners';
 import findValueByKey from 'utils/findValueByKey';
-import { mainColor } from '../material-dashboard-pro-react';
+import { lightGrayColor } from '../material-dashboard-pro-react';
 import ModalStyle from './ModalStyle';
 
 
@@ -13,7 +13,7 @@ const Loading = (props) => {
   const { classes, loading } = props;
   return loading ? (
     <div className={classes.cover}>
-      <PropagateLoader color={mainColor} size={40} />
+      <PropagateLoader color={lightGrayColor} size={38} />
     </div>
   ) : null;
 };
@@ -22,23 +22,10 @@ Loading.propTypes = {
   loading: PropTypes.bool.isRequired,
   classes: PropTypes.objectOf(PropTypes.any).isRequired,
 };
-//
-// const mapStateToProps = state => ({
-//   loading: !!Object.keys(state).reduce(
-//     (acc, cur) => (acc || state[cur].isLoading), false,
-//   ),
-// });
 
 const mapStateToProps = (state) => {
   const loading = [];
-  console.log('this is state you finding', state);
   findValueByKey(state, 'isLoading', loading);
-  console.log('loading', loading);
-  console.log('reducer', loading.reduce((final, current) => {
-    console.log('final', final);
-    console.log('current', current);
-    return (final || current);
-  }, false));
   return ({
     loading: loading.reduce((final, current) => (final || current), false),
   });
