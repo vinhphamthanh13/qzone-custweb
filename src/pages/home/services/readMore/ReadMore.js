57e67b6ec3866de037d2a6eed5637454dfa433ce
DavidPham <david.vinhphamthanh@gmail.com>
2019-02-23 17:01:33 +0700

    [UI/UX] Implement the Read More component

---
+++ src/pages/home/services/readMore/ReadMore.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import {
+  func, bool, string, number,
+} from 'prop-types';
+import { AccessTime, LocationOn } from '@material-ui/icons';
+import {
+  Button, Dialog, DialogActions, DialogContent, DialogContentText,
+  DialogTitle, Typography,
+} from '@material-ui/core';
+import CustomLink from 'components/CustomLink';
+import RateStar from '../rating/RateStar';
+import styles from './ReadMore.module.scss';
+
+const ReadMore = (props) => {
+  const {
+    isOpen, onClose, serviceName, duration, orgName, orgDescription, orgId,
+  } = props;
+  return (
+    <Dialog
+      open={isOpen}
+      onClose={onClose}
+      aria-labelledby="description-dialog"
+      classes={{ paper: styles.readMorePaper }}
+    >
+      <DialogTitle id="description-dialog">
+        <Typography variant="subheading" classes={{ subheading: styles.readMoreTitle }} color="primary">
+          {serviceName}
+        </Typography>
+        <RateStar rating={5} reviews={4000} />
+        <div className={styles.blockItem}>
+          <div className={styles.iconInfo}>
+            <AccessTime className={styles.icon} />
+            <Typography variant="body1" color="primary">{duration} minutes</Typography>
+          </div>
+          <div className={styles.iconInfo}>
+            <LocationOn className={styles.icon} />
+            <Typography variant="body1">
+              <CustomLink text={orgName} to={`/organisation/${orgId}`} />
+            </Typography>
+          </div>
+        </div>
+      </DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          <div className={styles.description}>
+            {orgDescription || ''}
+          </div>
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onClose} color="primary" variant="outlined">
+          Close
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+ReadMore.propTypes = {
+  isOpen: bool.isRequired,
+  onClose: func.isRequired,
+  serviceName: string.isRequired,
+  duration: number.isRequired,
+  orgName: string.isRequired,
+  orgId: string.isRequired,
+  orgDescription: string.isRequired,
+};
+
+export default ReadMore;
--- src/pages/home/services/serviceCard/ServiceDetail.js
+++ src/pages/home/services/serviceCard/ServiceDetail.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import {
-  Typography, Button,
-  Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions,
+  Typography,
+  // Button,
+  // Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions,
 } from '@material-ui/core';
 import { AccessTime, LocationOn } from '@material-ui/icons';
 import { serviceType } from 'types/global';
 import CustomLink from 'components/CustomLink';
 import styles from './ServiceDetail.module.scss';
 import RateStar from '../rating/RateStar';
+import ReadMore from '../readMore/ReadMore';
 
 export default class ServiceDetail extends React.PureComponent {
   constructor(props) {
@@ -30,44 +32,16 @@ export default class ServiceDetail extends React.PureComponent {
     const { service } = this.props;
     const { isDialogDescOpen } = this.state;
     return (
-      <React.Fragment>
-        <Dialog
-          open={isDialogDescOpen}
+      <>
+        <ReadMore
+          isOpen={isDialogDescOpen}
           onClose={this.handleClose}
-          aria-labelledby="description-dialog"
-          classes={{ paper: styles.readMorePaper }}
-        >
-          <DialogTitle id="description-dialog">
-            <Typography variant="subheading" classes={{ subheading: styles.readMoreTitle }} color="primary">
-              {service.name}
-            </Typography>
-            <RateStar rating={5} reviews={4000} />
-            <div className={styles.blockItem}>
-              <div className={styles.iconInfo}>
-                <AccessTime className={styles.icon} />
-                <Typography variant="body1" color="primary">{service.duration} minutes</Typography>
-              </div>
-              <div className={styles.iconInfo}>
-                <LocationOn className={styles.icon} />
-                <Typography variant="body1">
-                  <CustomLink text={service.organization.name} to={`/organisation/${service.organization.id}`} />
-                </Typography>
-              </div>
-            </div>
-          </DialogTitle>
-          <DialogContent>
-            <DialogContentText>
-              <div className={styles.description}>
-                {service.description || ''}
-              </div>
-            </DialogContentText>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={this.handleClose} color="primary" variant="outlined">
-              Close
-            </Button>
-          </DialogActions>
-        </Dialog>
+          serviceName={service.name}
+          duration={service.duration}
+          orgName={service.organization.name}
+          orgId={service.organization.id}
+          orgDescription={service.description || ''}
+        />
         <Typography
           variant="subheading"
           classes={{ subheading: styles.title }}
@@ -99,7 +73,7 @@ export default class ServiceDetail extends React.PureComponent {
             </Typography>
           </div>
         </div>
-      </React.Fragment>
+      </>
     );
   }
 }
