f0bb152fa8b086ab4d889abb5cc63571ca32fa3d
robert.raufer@gmail.com <robert@2018>
2019-02-25 20:19:53 +0700

    using onboarding pool

--- src/config/auth.js
+++ src/config/auth.js
@@ -5,14 +5,14 @@ export const GOOGLE_ID = '166981643559-r54fbu1evv6cpfpphqjtlo4j950vdmvn.apps.goo
 export const AWS_CONFIG = {
   Auth: {
     // REQUIRED - Amazon Cognito Identity Pool ID
-    identityPoolId: 'ap-southeast-2:fae931b0-0397-4d09-99fe-5db825fad329',
+    identityPoolId: 'ap-southeast-2:adab2657-684c-4222-a17a-9a82b6a5ee84',
     // REQUIRED - Amazon Cognito Region
     region: 'ap-southeast-2',
     identityPoolRegion: 'ap-southeast-2',
     // OPTIONAL - Amazon Cognito User Pool ID
-    userPoolId: 'ap-southeast-2_k3Ly7reYV',
+    userPoolId: 'ap-southeast-2_0sAegznUX',
     // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
-    userPoolWebClientId: '5bip0lat6r7tf023qs5a9qd04p',
+    userPoolWebClientId: '3ov1blo2eji4acnqfcv88tcidn',
     // OPTIONAL - Enforce user authentication prior to accessing AWS resources or not
     mandatorySignIn: true,
   },
