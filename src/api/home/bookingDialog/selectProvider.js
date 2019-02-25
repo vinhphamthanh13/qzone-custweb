d06b9f5912d2cccc910f21492932bcadfb4224cc
robert.raufer@gmail.com <robert@2018>
2019-02-25 22:15:15 +0700

    change providers -> users

--- src/api/home/bookingDialog/selectProvider.js
+++ src/api/home/bookingDialog/selectProvider.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 export const searchProvidersByService = serviceId => axios.get(`/service-providers-by-serviceid/${serviceId}`);
 
-export const searchProviderById = providerId => axios.get(`/providers/${providerId}`);
+export const searchProviderById = providerId => axios.get(`/users/${providerId}`);
 
 export const findAvailabilitiesByDateRange = body => axios.post('/find-availabilities-by-date-range', body);
