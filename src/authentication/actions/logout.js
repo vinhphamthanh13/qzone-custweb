d07b8a54088e4eeccf1ff1a90d3329a8c7ec1ec2
DavidPham <david.vinhphamthanh@gmail.com>
2019-03-02 23:33:23 +0700

    [UI/UX] Enforce the Google SignOut

--- src/authentication/actions/logout.js
+++ src/authentication/actions/logout.js
@@ -1,9 +1,10 @@
 // Logout
 
 import { Auth } from 'aws-amplify';
-import { saveSession } from 'config/localStorage';
+import { saveSession, loadSession } from 'config/localStorage';
+import { setLoading } from 'actions/common';
+import { AUTH_METHOD } from 'config/auth';
 import { LOGOUT_ERROR, LOGOUT_SUCCESS } from './constants';
-import { setLoading } from '../../actions/common';
 
 const initSession = {};
 
@@ -18,15 +19,29 @@ const logoutError = payload => ({
 });
 
 export const logout = () => (dispatch) => {
-  dispatch(setLoading(true));
-  Auth.signOut({ global: true })
-    .then((data) => {
-      dispatch(setLoading(false));
-      dispatch(logoutSuccess(data));
-      saveSession(initSession);
-    })
-    .catch((error) => {
-      dispatch(setLoading(false));
-      dispatch(logoutError(error));
-    });
+  const currentSession = loadSession();
+  if (currentSession && (currentSession.isAuthenticated || currentSession.qz_token)) {
+    const { provider } = currentSession;
+    dispatch(setLoading(true));
+    if (provider && provider === 'google') {
+      const ga = window.gapi[AUTH_METHOD].getAuthInstance();
+      ga.signOut().then(() => {
+        ga.disconnect();
+        dispatch(logoutSuccess());
+        dispatch(setLoading(false));
+        saveSession(initSession);
+      });
+    } else {
+      Auth.signOut({ global: true })
+        .then((data) => {
+          dispatch(logoutSuccess(data));
+          dispatch(setLoading(false));
+          saveSession(initSession);
+        })
+        .catch((error) => {
+          dispatch(logoutError(error));
+          dispatch(setLoading(false));
+        });
+    }
+  }
 };
