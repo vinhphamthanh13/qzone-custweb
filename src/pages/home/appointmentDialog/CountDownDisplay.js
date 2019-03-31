9faae3da82baf089e5fcfb0275a328bd8e68779d
DavidPham <david.vinhphamthanh@gmail.com>
2019-03-31 16:09:50 +0700

    [UI/UX] Reset count down to 0 when  completed!

--- src/pages/home/appointmentDialog/CountDownDisplay.js
+++ src/pages/home/appointmentDialog/CountDownDisplay.js
@@ -5,10 +5,14 @@ import CountDown from 'react-countdown-now';
 import CustomModal from 'components/Modal/CustomModal';
 
 class CountDownDisplay extends Component {
-  state = {
-    isStartingCountDown: false,
-    isStoppingCountDown: false,
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      isStartingCountDown: false,
+      isStoppingCountDown: false,
+      startCountDown: props.startTime * 60 * 1000,
+    };
+  }
 
   handleCloseStartPopUp = () => {
     this.setState({
@@ -28,6 +32,7 @@ class CountDownDisplay extends Component {
     this.setState({
       isStoppingCountDown: false,
       isStartingCountDown: false,
+      startCountDown: 0,
     });
   };
 
@@ -39,9 +44,8 @@ class CountDownDisplay extends Component {
   };
 
   render() {
-    const { startTime, serviceName, providerName } = this.props;
-    const { isStartingCountDown, isStoppingCountDown } = this.state;
-    const startCountDown = startTime * 60 * 1000; // in minute
+    const { serviceName, providerName } = this.props;
+    const { isStartingCountDown, isStoppingCountDown, startCountDown } = this.state;
     const startCountDownPopup = isStartingCountDown
       ? (
         <CustomModal
