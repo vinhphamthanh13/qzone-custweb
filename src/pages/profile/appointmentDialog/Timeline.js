02a638d0aa59ceef833c04718a91f8d1f899f14f
DavidPham <david.vinhphamthanh@gmail.com>
2019-10-11 01:56:36 +0700

    [OrgLink] Sort event list of profile

--- src/pages/profile/appointmentDialog/Timeline.js
+++ src/pages/profile/appointmentDialog/Timeline.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import { arrayOf, any } from 'prop-types';
+import moment from 'moment';
 import { VerticalTimeline } from 'react-vertical-timeline-component';
 import { EVENT_STATUS } from 'utils/constants';
 import TimelineCard from './TimelineCard';
 import s from './Timeline.module.scss';
 
 const Timeline = ({ items }) => {
-  const cancelledEvent = items.filter(item => item.status === EVENT_STATUS.CANCELED)
+  const cancelledEvent = items
+    .filter(item => item.status === EVENT_STATUS.CANCELED)
     .sort((a, b) => b.startSec - a.startSec);
-  const sortEvent = items.filter(item => item.status !== EVENT_STATUS.CANCELED).sort((a, b) => a.startSec - b.startSec);
+  const expiredEvent = items
+    .filter(item => moment().isAfter(item.providerStartSec))
+    .sort((a, b) => b.startSec - a.startSec);
+  const activeEvent = items
+    .filter(item => moment().isSameOrBefore(item.providerStartSec))
+    .sort((a, b) => a.startSec - b.startSec);
+  console.log('cancelledEvent: ', cancelledEvent);
+  console.log('expiredEvent: ', expiredEvent);
+  console.log('activeEvent: ', activeEvent);
+
   return (
     <VerticalTimeline className={s.verticalTimeline}>
-      {[...sortEvent, ...cancelledEvent].map(event => (<TimelineCard key={event.id} eventById={event} />))}
+      {[...activeEvent, ...expiredEvent, ...cancelledEvent]
+        .map(event => (<TimelineCard key={event.id} eventById={event} />))
+      }
     </VerticalTimeline>
   );
 };
